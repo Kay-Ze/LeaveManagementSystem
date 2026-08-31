91c9a3c5d55fad48644c7e696f68d9d82aed59bc
Nikunj Kapil <nikunjkapil279@gmail.com>
2026-09-01 01:01:27 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+const App = () => {
+  return (
+    <div>
+      <div
+      className='bg-amber-400 h-50 w-50'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
